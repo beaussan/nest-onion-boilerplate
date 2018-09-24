ce5b08e1e0b73a1bc31d7f9b4a5e8c50d0b2de30
Nicolas Beaussart <nic.beaussart@gmail.com>
2018-09-24 20:38:07 +0200

    :sparkles: added current user and role guard / decorators

---
+++ src/decorators/currentUser.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+  (data: any, req: any): any => { // TODO CHANGE TO USER WHEN USER IS DONE
+    return req.user;
+  },
+);
