56380db87bbd7072a33313ca05c1256ef0db42dd
Nicolas Beaussart <nic.beaussart@gmail.com>
2019-10-01 11:47:01 +0200

    :white_check_mark: fixing ping e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,14 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ping (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/ping')
       .expect(200)
-      .expect('Hello World!');
+      .expect('pong');
   });
+
+  afterAll(async () => {
+    await app.close();
+  })
 });
