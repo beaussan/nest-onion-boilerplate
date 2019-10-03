803b96f8cf969c9b7016481332ca608bd70277e6
Nicolas Beaussart <nic.beaussart@gmail.com>
2019-10-03 12:41:52 +0200

    :white_check_mark: adding test for crypto service

--- src/modules/core/crypto/crypto.service.spec.ts
+++ src/modules/core/crypto/crypto.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptoService } from './crypto.service';
+import * as argon from 'argon2';
 
 describe('CryptoService', () => {
   let service: CryptoService;
@@ -11,5 +12,21 @@ describe('CryptoService', () => {
   });
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should hash a password and verify if it is correct', async () => {
+    const toHash = 'some random string';
+
+    const hashed = await service.hash(toHash);
+
+    expect(await service.compare(toHash, hashed)).toBeTruthy();
+  });
+
+  it('should hash a password and return false if not same password', async () => {
+    const toHash = 'some random string';
+
+    const hashed = await service.hash(toHash);
+
+    expect(await service.compare(toHash + 'A', hashed)).toBeFalsy();
   });
 });
