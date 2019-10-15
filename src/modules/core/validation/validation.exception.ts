8624838ad3ef5dc4235b5a72f1ccbc3f1c18b2a1
Nicolas Beaussart <nic.beaussart@gmail.com>
2019-10-15 11:03:13 +0200

    :alien: Changed http validation exception constructor

--- src/modules/core/validation/validation.exception.ts
+++ src/modules/core/validation/validation.exception.ts
@@ -1,10 +1,9 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
-import { createHttpExceptionBody } from '@nestjs/common/utils/http-exception-body.util';
 
 export class ValidationException extends HttpException {
   constructor(errors: string | object | any) {
     super(
-      createHttpExceptionBody(
+      HttpException.createBody(
         errors,
         'VALIDATION_EXCEPTION',
         HttpStatus.BAD_REQUEST,
